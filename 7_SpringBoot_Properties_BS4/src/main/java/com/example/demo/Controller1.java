26472bbae42cc48750eea2dc1e512bfce7b42cbc
Luis Mendoza <luis.mendoza@bosonit.com>
2021-12-20 13:07:39 +0100

    Fin Ej7 - SO variable

--- 7_SpringBoot_Properties_BS4/src/main/java/com/example/demo/Controller1.java
+++ 7_SpringBoot_Properties_BS4/src/main/java/com/example/demo/Controller1.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
