feaecd4c191e6a4697fc5ba6a5410292bbed0357
Luis Mendoza <luis.mendoza@bosonit.com>
2021-12-22 12:02:40 +0100

    Ej 10 Fin

--- 10_Database_DB1/src/main/java/com/example/_Database_DB1/Controller.java
+++ 10_Database_DB1/src/main/java/com/example/_Database_DB1/Controller.java
@@ -34,15 +34,22 @@ public class Controller {
     public Usuario insert(@RequestBody Usuario usuario) throws Exception {
         if (usuario.getUsuario()==null) {throw new Exception("Usuario no puede ser nulo"); }
         if (usuario.getUsuario().length()>10) { throw new Exception("Longitud de usuario no puede ser superior a 10 caracteres");}
-        if (usuario.getPassword()==null) {throw new Exception("Usuario no puede ser nulo"); }
-        if (usuario.getName()==null) {throw new Exception("Usuario no puede ser nulo"); }
-        if (usuario.getCompany_email()==null) {throw new Exception("Usuario no puede ser nulo"); }
-        if (usuario.getCity()==null) {throw new Exception("Usuario no puede ser nulo"); }
-        if (usuario.getActive()==null) {throw new Exception("Usuario no puede ser nulo"); }
-        if (usuario.getCreated_date()==null) {throw new Exception("Usuario no puede ser nulo"); }
-        if (usuario.getPersonal_email()==null) {throw new Exception("Usuario no puede ser nulo"); }
+        if (usuario.getPassword()==null) {throw new Exception("getPassword no puede ser nulo"); }
+        if (usuario.getName()==null) {throw new Exception("getName no puede ser nulo"); }
+        if (usuario.getCompany_email()==null) {throw new Exception("getCompany_email no puede ser nulo"); }
+        if (usuario.getCity()==null) {throw new Exception("getCity no puede ser nulo"); }
+        if (usuario.getActive()==null) {throw new Exception("getActive no puede ser nulo"); }
+        if (usuario.getCreated_date()==null) {throw new Exception("getCreated_date no puede ser nulo"); }
+        if (usuario.getPersonal_email()==null) {throw new Exception("getPersonal_email no puede ser nulo"); }
 
         usuarioRepositorio.save(usuario);
         return usuario;
+    }
+
+    @DeleteMapping("/id/{id}")
+    public void deleteById(@PathVariable int id) throws Exception {
+        if(usuarioRepositorio.findById(id).isPresent()){
+            usuarioRepositorio.deleteById(id);
+        }
     }
 }
