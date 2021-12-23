000959a44d4e888648ef3dbe0be1a3907403ce50
Luis Mendoza <luis.mendoza@bosonit.com>
2021-12-23 13:23:20 +0100

    Add more

--- 11_Hexagon_BP1/src/main/java/com/example/_Database_DB1/Usuario/infrastructure/controller/SearchByIdUsuarioController.java
+++ 11_Hexagon_BP1/src/main/java/com/example/_Database_DB1/Usuario/infrastructure/controller/SearchByIdUsuarioController.java
@@ -3,6 +3,7 @@ package com.example._Database_DB1.Usuario.infrastructure.controller;
 import com.example._Database_DB1.Usuario.application.Port.GetUsuarioPort;
 import com.example._Database_DB1.Usuario.domain.Usuario;
 import com.example._Database_DB1.Usuario.domain.UsuarioRepositorio;
+import com.example._Database_DB1.Usuario.infrastructure.dto.output.UsuarioOutputDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,8 +15,8 @@ public class SearchByIdUsuarioController {
     GetUsuarioPort getUsuarioPort;
 
     @GetMapping("id/{id}")
-    public Usuario getById(@PathVariable int id) throws Exception{
-        return getUsuarioPort.getById(id);
+    public UsuarioOutputDTO getById(@PathVariable int id) throws Exception{
+        return new UsuarioOutputDTO(getUsuarioPort.getById(id));
     }
 
 }
