84f27a0c05c6f4525f8cb5fac3963ea9c17c714c
Luis Mendoza <luis.mendoza@bosonit.com>
2021-12-30 12:06:06 +0100

    Fin, in memory database

--- 14_RestTemplate_BS9/src/main/java/com/example/_Database_DB1/Persona/infrastructure/controller/Feign.java
+++ 14_RestTemplate_BS9/src/main/java/com/example/_Database_DB1/Persona/infrastructure/controller/Feign.java
@@ -1,8 +1,6 @@
 package com.example._Database_DB1.Persona.infrastructure.controller;
 
-import com.example._Database_DB1.Profesor.infrastructure.dto.output.SimpleProfesorOutputDTO;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
